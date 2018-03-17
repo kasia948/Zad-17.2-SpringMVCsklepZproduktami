92caa950185eb59281c82ddd5244cc06769399ae
rudyknowak <rudyknowak@gmail.com>
2018-03-17 12:45:30 +0100

    wersja poprawiona

--- src/main/java/pl/sklepzproduktami/demo/ProductController.java
+++ src/main/java/pl/sklepzproduktami/demo/ProductController.java
@@ -1,6 +1,7 @@
 package pl.sklepzproduktami.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -20,62 +21,41 @@ public class ProductController {
     @ResponseBody
     public String printAll() {
         String tekst = "";
-        double sumPrice=0;
+        double sumPrice = 0;
         for (int i = 0; i < datbas.produkty.size(); i++) {
             Product product = datbas.produkty.get(i);
             tekst += product.getName() + " " + product.getPrice() + " " + product.getCategory() + "<br>";
             sumPrice = sumPrice + product.getPrice();
 
         }
-        tekst+="<br/>Suma cen to: "+sumPrice;
+        tekst += "<br/>Suma cen to: " + sumPrice;
         return tekst;
     }
 
-    @RequestMapping("/spozywcze")
+    @RequestMapping("/lista")
     @ResponseBody
-    public String printSpozywcze() {
+    public String printAll(@RequestParam(value = "kategoria") String category) {
         String tekst = "";
-        double sumPrice=0;
+        double sumPrice = 0;
         for (int i = 0; i < datbas.produkty.size(); i++) {
             Product product = datbas.produkty.get(i);
-            if (product.getCategory().equals("spożywczy")) {
+            if (product.getCategory().equals(category)) {
                 tekst += product.getName() + " " + product.getPrice() + " " + product.getCategory() + "<br>";
                 sumPrice = sumPrice + product.getPrice();
             }
         }
-        tekst+="<br/>Suma cen to: "+sumPrice;
+        tekst += "<br/>Suma cen to: " + sumPrice;
         return tekst;
     }
 
-    @RequestMapping("/domowe")
-    @ResponseBody
-    public String printDomowe() {
-        String tekst = "";
-        double sumPrice=0;
-        for (int i = 0; i < datbas.produkty.size(); i++) {
-            Product product = datbas.produkty.get(i);
-            if (product.getCategory().equals("domowy")) {
-                tekst += product.getName() + " " + product.getPrice() + " " + product.getCategory() + "<br>";
-                sumPrice = sumPrice + product.getPrice();
-            }
-        }
-        tekst+="<br/>Suma cen to: "+sumPrice;
-        return tekst;
-    }
-
-    @RequestMapping("/inne")
-    @ResponseBody
-    public String printInne() {
-        String tekst = "";
-        double sumPrice=0;
-        for (int i = 0; i < datbas.produkty.size(); i++) {
-            Product product = datbas.produkty.get(i);
-            if (product.getCategory().equals("inny")) {
-                tekst += product.getName() + " " + product.getPrice() + " " + product.getCategory() + "<br>";
-                sumPrice = sumPrice + product.getPrice();
-            }
-        }
-        tekst+="<br/>Suma cen to: "+sumPrice;
-        return tekst;
+    @RequestMapping("/add")
+    public String addProduct(@RequestParam(value = "nazwa") String name, @RequestParam(value = "cena", required = false, defaultValue = "0") String priceString, @RequestParam(value = "kategoria", required = false, defaultValue = "domowy") String category) {
+        int parseInt = Integer.parseInt(priceString);
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(parseInt);
+        product.setCategory(category);
+        datbas.produkty.add(product);
+        return "redirect:/all";
     }
 }
